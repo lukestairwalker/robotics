1656b2e2ddb82fd262c8ce903624a06e92c490ed
Jan Schultze <jan.l.schultze@gmail.com>
2025-07-18 20:31:45 +0200

    added stepper motor

---
+++ steppermotor/steppermotor.ino/steppermotor.ino
@@ -0,0 +1,17 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;
+
+// Beispiel für Reihenfolge: IN1=8, IN2=10, IN3=9, IN4=11
+Stepper stepper(stepsPerRevolution, 8, 10, 9, 11);
+
+void setup() {
+  stepper.setSpeed(10); // langsam anfangen
+}
+
+void loop() {
+  stepper.step(stepsPerRevolution); // eine Umdrehung vorwärts
+  delay(1000);
+  stepper.step(-stepsPerRevolution); // eine Umdrehung rückwärts
+  delay(1000);
+}
